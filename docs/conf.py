0bf88cf404920a182b643bddf5bf9ed34214ebdc
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2020-03-23 11:05:48 +0100

    finalize move jekyll->sphinx

---
+++ docs/conf.py
@@ -0,0 +1,62 @@
+"""Configure details for documentation with sphinx."""
+from datetime import date
+
+import sphinx_bootstrap_theme
+
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+]
+
+# General information about the project.
+project = ''
+author = 'Stefan Appelhoff'
+year = '2020'
+td = date.today()
+if year != str(td.year):
+    year = '{}-{}'.format(year, td.year)
+
+copyright = '{}, Stefan Appelhoff. Last updated {}'.format(year, td.isoformat())
+
+html_show_sphinx = False  # do not show "created using Sphinx X.Y"
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = ''
+
+# The full version, including alpha/beta/rc tags.
+release = version
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This patterns also effect to html_static_path and html_extra_path
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# HTML options (e.g., theme)
+# see: https://sphinx-bootstrap-theme.readthedocs.io/en/latest/README.html
+# Clean up sidebar: Do not show "Source" link
+html_show_sourcelink = False
+
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    'navbar_title': '',
+    'bootswatch_theme': "flatly",
+    'navbar_sidebarrel': False,  # no "previous / next" navigation
+    'navbar_pagenav': False,  # no "Page" navigation in sidebar
+    'bootstrap_version': "3",
+    'navbar_links': [
+        ("GitHub", "https://github.com/sappelhoff/", True),
+        ("Twitter", "https://twitter.com/stefanappelhoff", True),
+        ("SoundCloud", "https://soundcloud.com/stefan-appelhoff", True),
+        ("Google Scholar", "https://scholar.google.de/citations?user=ceECtQIAAAAJ&hl=en", True),  # noqa: E501
+    ]}
---
+++ docs/index.rst
@@ -0,0 +1,34 @@
+Stefan Appelhoff
+================
+
+Hi, I am Stefan.
+
+I am currently pursuing my PhD in cognitive neuroscience at the
+`Max Planck Institute for Human Development <https://www.mpib-berlin.mpg.de/en/staff/stefan-appelhoff>`_
+in Berlin:
+
+
+* How do humans search for information before making a value based choice?
+* When do they decide to stop gathering additional information?
+* How is active information accumulation different from passive information
+  accumulation?
+
+Next to figuring out these questions, I am contributing to the
+`Brain Imaging Data Structure (BIDS) <https://bids.neuroimaging.io>`_\ ,
+an emerging standard for the organization and documentation of neuroimaging
+data. BIDS will help making neuroscience more reproducible by facilitating
+(re)use, sanity-checking, and sharing of scientific data.
+
+I am also interested in more technical and concrete problems, such as how to
+accurately mark events in an experiment using consumer grade hardware.
+
+Finally, I am a strong proponent of open source software and contribute to
+several software packages such as:
+
+
+* `MNE-Python <https://github.com/mne-tools/mne-python>`_
+* `MNE-BIDS <https://github.com/mne-tools/mne-bids>`_
+* `BIDS-validator <https://github.com/bids-standard/bids-validator>`_
+
+If you want to know more, or collaborate on an interesting topic, please get in
+touch!
